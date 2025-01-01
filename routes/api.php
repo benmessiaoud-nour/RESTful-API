25f42ac83e5aa1e4fc49a12c7b10fe82ac9fb959
benmessiaoud nour <benmessiaoud.nour@example.com>
2025-01-01 22:35:27 +0100

    setting up routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Lesson;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +17,27 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('lessons',function(){
+    return \App\Models\Lesson::all();
+});
+
+Route::get('lessons/{id}', function($id){
+    return \App\Models\Lesson::find($id);
+});
+
+Route::post('lessons' , function(Request $request){
+    return Lesson::create($request->all());
+});
+
+Route::put('lessons/{id}', function(Request $request, $id){
+   $Lesson =  Lesson::findOrFail($id);
+   $Lesson->update($request->all());
+   return $Lesson;
+});
+
+Route::delete('lessons/{id}' , function($id){
+    \App\Models\Lesson::find($id)->delete();
+    return 204;
 });
